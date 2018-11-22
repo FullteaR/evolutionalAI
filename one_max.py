50b707b551edb5105440b8fd57feacb0098a2ea3
Rikuta Furuta <rikuta@furutan.com>
2018-11-23 00:30:56 +0900

    fix: tiny things

--- one_max.py
+++ one_max.py
@@ -2,21 +2,27 @@ import random
 import copy
 import numpy as np
 
-GENOM_LENGTH = 100
-MAX_GENOM_LIST = 1000
-SELECT_GENOM = 200
+GENOM_LENGTH = 10
+MAX_GENOM_LIST = 10000
+SELECT_GENOM = 2000
 INDIVIDUAL_MUTATION = 0.1  # 個体に突然変異が起きる確率を表す
 GENOM_MUTATION = 0.1  # 突然変異が起きた個体について、各々の遺伝子が変異する確率を表す
-MAX_ITER = 50
-ATGC = [0, 1, 2, 3]
+MAX_ITER = 5000
+ATGC = [i for i in range(10)]
 
 
 class Gene:
     def __init__(self):
         self.chromosome = [random.choice(ATGC) for i in range(GENOM_LENGTH)]
 
+
     def __repr__(self):
         return "<<{}>>".format(",".join([str(i) for i in self.chromosome]))
+
+    def mutate(self):
+        for i in range(len(self.chromosome)):
+            if random.random() < GENOM_MUTATION:
+                self.chromosome[i] = random.choice(ATGC)
 
 
 class Ecoli:
@@ -25,17 +31,11 @@ class Ecoli:
 
     def evaluate(self):
         chromosome_np = np.asarray(self.gene.chromosome)
-        return 1 / (np.sum((chromosome_np - 1)**2)+1e-5)#すべてが1であるほどよい
+        return 1 / (np.sum((chromosome_np - np.arange(10))**2)+1e-5)#連番
 
     def mutate(self, force=False):
         if force or random.random() < INDIVIDUAL_MUTATION:
-            length = len(self.gene.chromosome)
-            for i in range(length):
-                if random.random() < GENOM_MUTATION:
-                    self.gene.chromosome[i] = random.choice(ATGC)
-            return True
-        else:
-            return False
+            self.gene.mutate()
 
 
 def crossOver(Ecoli1, Ecoli2, i=None, j=None):
@@ -78,15 +78,22 @@ def selectEcols(pool):
 for epoch in range(MAX_ITER):
     print("epoch:{}".format(epoch))
     pool = selectEcols(pool)
-
-    print("Max:{}".format(pool[0].evaluate()))
+    Max=pool[0].evaluate()
+    print("Max:{}".format(Max))
     if pool[0].evaluate() > highScore:
-        highScore = pool[0].evaluate()
+        highScore = Max
         best = copy.deepcopy(pool[0])
-    print("Min:{}".format(pool[-1].evaluate()))
+    Min=pool[-1].evaluate()
+    print("Min:{}".format(Min))
+    if Max-Min<1e-10:
+        for i in range(int(len(pool)*0.8)):
+            pool[i].mutate(force=True)
     pool = nextGeneration(pool)
     for ecol in pool:
         ecol.mutate()
+
+    if Max>10000:
+        break
 
     print()
 
